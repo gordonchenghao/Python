c12eb873872291888f6947526c1fbb975707dc78
Gordon Gao <chenghaogordon@gmail.com>
2022-05-05 17:16:52 +1000

    vsdf

---
+++ test.py
@@ -0,0 +1,8 @@
+student={'name':'Tom','major':'IT','age':'18'}
+student['height']=165
+student.update({"wight":80})
+print(student)
+student.pop('name')
+print(student)
+student.popitem()
+print(student)
